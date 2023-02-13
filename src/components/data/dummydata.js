04d87cead1a910bb0e9edb94e21eaaca17bf7ef1
diossyaban <54267318+Diossyaban@users.noreply.github.com>
2023-02-14 01:29:51 +0700

    Dikit lagi kelar

---
+++ src/components/data/dummydata.js
@@ -0,0 +1,236 @@
+import { Settings, CropRotate, ViewInAr, PieChart, Code, BarChart, CloudOutlined, FavoriteBorder, Public, PersonOutlined, AddLocationAltOutlined, PhoneIphone, EmailOutlined, Facebook, Twitter, Instagram, YouTube } from "@mui/icons-material"
+
+export const navlink = [
+  {
+    url: "/",
+    text: "Home",
+  },
+  {
+    url: "/about",
+    text: "About",
+  },
+  {
+    url: "/services",
+    text: "Services",
+  },
+  {
+    url: "/portfolio",
+    text: "Portfolio",
+  },
+  {
+    url: "/testimonials",
+    text: "Testimonials",
+  },
+  {
+    url: "/blog",
+    text: "Blog",
+  },
+  {
+    url: "/contact",
+    text: "Contact",
+  },
+]
+export const home = [
+  {
+    text: "HELLO I'M",
+    name: "Ichwaldi Dios Sya'Ban",
+    post: " Software Developer",
+    design: ".NET Developer",
+    desc: "Design, develop, troubleshoot, debug, test, ensure browser compatibility and implement website code by using MS SQL Server and C#, and ASP.NET MVC, Javascript ",
+  },
+]
+export const about = [
+  {
+    desc: "As a recent graduate of Sriwijaya University Palembang majoring in Computer Engineering with a GPA  (3.20). I have good motivation for growing, eager to learn, and can work in a team and individually.",
+    desc1: "I am also now developing my skills in several areas including: technology, networking, computers, music, cryptocurency. I am currently looking for a job in the world of computer technology, industry, mechanics, machinery, mining, because I want to develop the skills I have.",
+    cover: "./images/man.png",
+  },
+]
+export const services = [
+  {
+    id: 1,
+    icon: <Settings />,
+    title: "Freelance Gitaris ",
+    desc: "Nanti Saya Deskripsikan Lagi",
+  },
+  {
+    id: 2,
+    icon: <CropRotate />,
+    title: "Clean Code",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+  {
+    id: 3,
+    icon: <ViewInAr />,
+    title: "Responsive Design",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+  {
+    id: 4,
+    icon: <PieChart />,
+    title: "Material UI	",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+  {
+    id: 5,
+    icon: <Code />,
+    title: "Material UI Icons",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+  {
+    id: 6,
+    icon: <BarChart />,
+    title: "Awesome Support",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+]
+export const project = [
+  {
+    id: 1,
+    icon: <CloudOutlined />,
+    num: "2",
+    title: "HAPPY CLIENTS",
+  },
+  {
+    id: 2,
+    icon: <FavoriteBorder />,
+    num: "3",
+    title: "PROJECTS COMPLEATED",
+  },
+  {
+    id: 3,
+    icon: <Public />,
+    num: "12",
+    title: "FILES DOWNLOADED",
+  },
+  {
+    id: 4,
+    icon: <PersonOutlined />,
+    num: "1446",
+    title: "LIENS OF CODE",
+  },
+]
+export const portfolio = [
+  {
+    id: 1,
+    cover: "../images/port/port1.jpg",
+    name: "Brand",
+    category: "marketing",
+    title: "Brex Logo",
+  },
+  {
+    id: 2,
+    cover: "../images/port/port2.jpg",
+    name: "Brand",
+    category: "design",
+    title: "Brex Logo",
+  },
+  {
+    id: 3,
+    cover: "../images/port/port3.jpg",
+    name: "Brand",
+    category: "development",
+    title: "Brex Logo",
+  },
+  {
+    id: 4,
+    cover: "../images/port/port4.jpg",
+    name: "Brand",
+    category: "marketing",
+    title: "Brex Logo",
+  },
+  {
+    id: 5,
+    cover: "../images/port/port5.jpg",
+    name: "Brand",
+    category: "design",
+    title: "Brex Logo",
+  },
+  {
+    id: 6,
+    cover: "../images/port/port6.jpg",
+    name: "Brand",
+    category: "development",
+    title: "Brex Logo",
+  },
+]
+export const testimonials = [
+  {
+    id: 1,
+    text: "Dios Anaknya Baik Kok",
+    image: "./images/testimonials/team-1.png",
+    name: "Irham Rafi",
+    post: ".NET Developer",
+  },
+  {
+    id: 2,
+    text: "Teman Ngobrol Yang Baik Banget",
+    image: "./images/testimonials/team-2.png",
+    name: "Anonymous",
+    post: "Mantan Pacarnya",
+  },
+  {
+    id: 3,
+    text: "Main Gitarnya Jago Gess",
+    image: "./images/testimonials/team-3.png",
+    name: "Anonymous",
+    post: "Penggemar Rahasia",
+  },
+]
+export const blog = [
+  {
+    id: 1,
+    title: " Cara Belajar ASP.NET MVC",
+    date: "Feb 12, 2023",
+    author: "Diossyaban",
+    desc: "Nanti Saya Upload",
+    cover: "./images/blog/b1.jpg",
+  },
+  {
+    id: 2,
+    title: "Tuntas Kerjain Hackerank Solution",
+    date: "Feb 13, 2023",
+    author: "Diossyaban ",
+    desc: "Nanti Saya Upload",
+    cover: "./images/blog/b2.jpg",
+  },
+  {
+    id: 3,
+    title: "Apakah .NET 2023 Akan Mati?",
+    date: "Feb 14, 2023",
+    author: "Diossyaban ",
+    desc: "Nanti Saya Upload",
+    cover: "./images/blog/b3.png",
+  },
+]
+export const contact = [
+  {
+    icon: <AddLocationAltOutlined />,
+    text1: "Jl. Perjuangan Raya Kebun Jeruk",
+    text2: "Jakarta, Indonesia ",
+  },
+  {
+    icon: <PhoneIphone />,
+    text1: "+6281369302158",
+    text2: "+62895700834429",
+  },
+  {
+    icon: <EmailOutlined />,
+    text1: "ichwaldidiossyaban@gmail.com",
+    text2: "diossyaban17@gmail.com",
+  },
+]
+export const social = [
+  {
+    icon: <Facebook />,
+  },
+  {
+    icon: <Twitter />,
+  },
+  {
+    icon: <Instagram />,
+  },
+  {
+    icon: <YouTube />,
+  },
+]
